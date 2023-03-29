f2da75f676d9233731d098598df23d7cb5ad87e0
Serdar Kilicli <123742228+SerdarKlc1@users.noreply.github.com>
2023-03-29 22:51:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [notes, setNotes] = useState(()=>JSON.parse(localStorage.getItem("notes"??"[]")));
+  const [notes, setNotes] = useState([]);
 
-  useEffect(()=>{
-    localStorage.setItem("notes",JSON.stringify(notes))
-    ;
-    if(notes.length===0){document.title = "Keeper App";}
-    else if (notes.length===1){document.title="1 note"}
-    else if (notes.length>=2){
-        document.title=`${notes.length} notes`
-    }
-    }, [notes]);
   function addNote(newNote) {
     setNotes(prevNotes => {
       return [...prevNotes, newNote];
